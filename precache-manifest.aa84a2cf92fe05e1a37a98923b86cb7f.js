90774c542163b5c8b68105c0e91e58749b4c254f
Adam Hałdaś <adam.haldas@gmail.com>
2020-09-24 22:09:34 +0200

    Updates

---
+++ precache-manifest.aa84a2cf92fe05e1a37a98923b86cb7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a2a0a5be89b54b591e2",
+    "url": "/ReactFormValidation/static/css/main.b11ffb91.chunk.css"
+  },
+  {
+    "revision": "5a2a0a5be89b54b591e2",
+    "url": "/ReactFormValidation/static/js/main.5a2a0a5b.chunk.js"
+  },
+  {
+    "revision": "d439e8744bee3815eb5d",
+    "url": "/ReactFormValidation/static/js/1.d439e874.chunk.js"
+  },
+  {
+    "revision": "1910f3318ec8bbc02f60",
+    "url": "/ReactFormValidation/static/js/runtime~main.1910f331.js"
+  },
+  {
+    "revision": "83cb56b7a512f41dbbf2ce3f4d94c08a",
+    "url": "/ReactFormValidation/index.html"
+  }
+];
